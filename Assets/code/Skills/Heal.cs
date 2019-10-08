5e6b548eda931b5641a480f9a6623e80a5034382
cdom <dominguerilla@gmail.com>
2019-10-08 17:31:25 -0400

    Rearrange LIMB codebase

---
+++ Assets/code/Skills/Heal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LIMB;
+using System;
+
+[CreateAssetMenu(fileName = "Healing Skill", menuName = "Skill/Heal", order = 1)]
+public class Heal : Skill
+{
+    public override void Execute(Combatant actor, Combatant target) {
+        target.InflictDamage(new Damage(-10f));
+    }
+}
