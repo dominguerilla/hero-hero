3d2dedde84b2f7dc12dd4d0355479baf0bd72642
cdom <dominguerilla@gmail.com>
2020-06-06 15:17:25 -0400

    Add arms and crosshair to Player

--- Assets/code/Interactor.cs
+++ Assets/code/Interactor.cs
@@ -18,6 +18,11 @@ public class Interactor : MonoBehaviour
         inventory = this.GetComponent<InventoryComponent>();    
     }
 
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 10, 10), "");
+    }
+
     // Update is called once per frame
     void Update()
     {
